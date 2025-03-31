ec9d055ce862981c39da5a175d9ba80740c25858
Adeniyi Adejare <pelznotion@gmail.com>
2025-03-30 13:22:32 -1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherApp from "./components/weatherapp";
+import "./components/weatherapp.css";
+
+const App = () => {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+};
+
+export default App;
